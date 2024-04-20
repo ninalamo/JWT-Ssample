f05599022975f05cb3ae9189209bc3dc6cb63c08
Ninism <nin.alamo@outlook.com>
2024-04-20 09:10:35 +0800

    Added client project

---
+++ auth-server/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace auth_server.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult GetToken(string userName, string password)
+        {
+            return View();
+        }
+    }
+}
