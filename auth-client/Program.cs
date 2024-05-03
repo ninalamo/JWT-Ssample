62c63574062fd627061ff6830b611dfabbad255d
Ninism <nin.alamo@outlook.com>
2024-05-04 07:54:45 +0800

    Update Program.cs

--- auth-client/Program.cs
+++ auth-client/Program.cs
@@ -81,6 +81,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(o =>
+{
+    o.AllowAnyHeader();
+    o.AllowAnyMethod();
+    o.AllowAnyOrigin();
+});
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
